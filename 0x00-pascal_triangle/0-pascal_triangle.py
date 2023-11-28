e30e9e08764b79723c42efcb7621bfd236dfc423
Herman100 <hermankwamebour30@gmail.com>
2023-11-28 16:51:39 +0000

    pascal's triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,9 +4,6 @@ function that returns list of Pascal's triangle of n
 '''
 
 
-from math import factorial
-
-
 def pascal_triangle(n):
     """
     function for pascal's triangle
@@ -14,11 +11,12 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangle = []
-    for n in range(n):
-        row = []
-        for r in range(n + 1):
-            ncr = factorial(n) // (factorial(r) * factorial(n - r))
-            row.append(ncr)
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
         triangle.append(row)
+
     return triangle
