42df43a0f781c9d0b8735789774258453ba49e72
Herman100 <hermankwamebour30@gmail..com>
2024-02-15 02:47:35 -0800

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,27 +2,32 @@
 """Determine the winner of a prime number game."""
 
 
-def isWinner(x, nums):
+def prime_nums(n):
     """
     The name of the player who won or none for no clear winner.
     """
-    winners = []
-    for n in nums:
-        numbers = list(range(1, n+1))
-        turn = 'Maria'
-        while True:
-            primes = [num for num in numbers
-                      if all(num % i != 0 for i in
-                             range(2, int(num**0.5) + 1))]
-            if not primes:
-                winners.append('Ben' if turn == 'Maria' else 'Maria')
-                break
-            prime = min(primes)
-            numbers = [num for num in numbers if num % prime != 0]
-            turn = 'Ben' if turn == 'Maria' else 'Maria'
-    if winners.count('Maria') > winners.count('Ben'):
-        return 'Maria'
-    elif winners.count('Maria') < winners.count('Ben'):
-        return 'Ben'
-    else:
+    prime_numbers = []
+    filtered = [True] * (n + 1)
+    for prime in range(2, n + 1):
+        if (filtered[prime]):
+            prime_numbers.append(prime)
+            for i in range(prime, n + 1, prime):
+                filtered[i] = False
+    return prime_numbers
+
+
+def isWinner(x, nums):
+    """
+    returns the winner of the game
+    """
+    if x is None or nums is None or x == 0 or nums == []:
         return None
+    Maria = Ben = 0
+    for i in range(x):
+        prime_numbers = prime_nums(nums[i])
+        if len(prime_numbers) % 2 == 0:
+            Ben += 1
+        else:
+            Maria += 1
+    return 'Maria' if Maria > Ben else 'Ben'
+    return None
